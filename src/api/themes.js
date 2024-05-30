ef63e6427955e876a601dc0670f16cc35b415edf
Leonardo Arthur <leonardoarthu627@gmail.com>
2024-05-30 17:44:06 -0300

    Commitando para o gitpages

---
+++ src/api/themes.js
@@ -0,0 +1,17 @@
+        // Função para alternar entre os temas
+        function toggleTheme() {
+            if (document.documentElement.classList.contains('dark')) {
+                document.documentElement.classList.remove('dark');
+                localStorage.theme = 'light';
+            } else {
+                document.documentElement.classList.add('dark');
+                localStorage.theme = 'dark';
+            }
+        }
+
+        // Carregar o tema preferido do usuário ao carregar a página
+        if (localStorage.theme === 'dark' || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
+            document.documentElement.classList.add('dark');
+        } else {
+            document.documentElement.classList.remove('dark');
+        }
